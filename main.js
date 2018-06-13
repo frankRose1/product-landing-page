5a08858dcbd265f91e720f1c6fe5aaae078cf5cd
Frank Rosendorf <frank.rosendorf1@gmail.com>
2018-06-13 15:06:25 -0400

    added new pictures and media queries

--- main.js
+++ main.js
@@ -1,11 +1,23 @@
-// card section animations 
+$(document).ready(function(){
 
-
-function scrollTop() {
+//card animations
+function scrollDistance() {
 	const scrolled = window.scrollY;
 	if (scrolled >= 540) {
 		document.body.classList.add('scroll');
 	}
 }
 
-window.addEventListener('scroll', scrollTop);
+window.addEventListener('scroll', scrollDistance);
+
+$('a').on('click', function(e){
+	e.preventDefault();
+	const target = $(this.hash);
+	if(target.length > 0){
+		$('html, body').stop().animate({
+		scrollTop: target.offset().top - 10
+		}, 500);
+	}
+});
+
+}); //end doc ready
